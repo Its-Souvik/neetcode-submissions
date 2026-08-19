dadd6e7ac290241357afb1da0f1f0846e9a7ffc9
Souvik Ghosh <161873294+Its-Souvik@users.noreply.github.com>
2026-08-19 12:35:23 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice=prices[0];
+        int maxProfit=0;
+        int n=prices.length;
+
+        for(int i=1;i<n;i++){
+
+            minPrice=Math.min(minPrice,prices[i]);
+            int profit=prices[i]-minPrice;
+            maxProfit=Math.max(maxProfit, profit);
+        }
+        return maxProfit;
+        
+    }
+}
